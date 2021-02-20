a1c42b047d09ff199675178a52c6479ae5465bd8
wliu01 <57687551+heccinchomker@users.noreply.github.com>
2021-02-20 17:51:11 -0500

    commitus
    commitus

--- backend/src/recommender.py
+++ backend/src/recommender.py
@@ -1,11 +1,25 @@
 import math
 import tensorflow as tf
-import pandas as pd 
+
 from typing import List, Tuple, Dict
-import account
+from account import Account
+import manager
+from manager import AccountManager
+
+import pandas as pd
+
 
 class Recommender:
 
     name: str
-    userdata: List[account]
+    userdata: AccountManager
+
+
+
+
+
+
+
+
+
 
