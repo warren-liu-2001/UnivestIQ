ea65f24f2e7b4c11e9bf6f1859fe506e3f9e18ed
1chowdhurai <raiyaan.rahman01@gmail.com>
2021-02-21 01:06:02 -0500

    added an email text box

--- frontend/univestiq/react-front-end/src/App.js
+++ frontend/univestiq/react-front-end/src/App.js
@@ -2,18 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Container, Row, Col, Button, Alert, Breadcrumb, Card} from 'react-bootstrap'
+import {Container, Row, Col, Button, Alert, Breadcrumb, Card, Form} from 'react-bootstrap'
+//import { PieChart } from 'react-minimal-pie-chart';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
+      <h1 margin-bottom="10px"> UnivestIQ Demo</h1>
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
+        
+          <Form>
+            <Form.Group>
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control type="email" placeholder="Example@email.com"></Form.Control>
+              <Form.Text className="text-muted"> Don't worry, we'll never share your email</Form.Text>
+            </Form.Group>
+          </Form>
+        
         <Card className= "mb-3" style = {{ color: "#000"}}>
-          <Card.Img/>
+          <Card.Img src="./Pie-Chart.jpg"/>
           <Card.Body>
             <Card.Title>
               Card Example
@@ -24,14 +39,17 @@ function App() {
           </Card.Body>
         </Card>
         <Button> Test Button </Button>
-        <a
+     
+        
+        
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
