be4ffb0e89ffdfcdce4d20df1a6127df19584582
wliu01 <57687551+heccinchomker@users.noreply.github.com>
2021-02-20 23:10:48 -0500

    fucking backend

--- backend/src/database.py
+++ backend/src/database.py
@@ -11,6 +11,7 @@ database_co = "mature-otter-855.defaultdb"
 password = "UofTHacks2021"
 
 import psycopg2
+from account import Account
 
 conn = psycopg2.connect(
     database=database_co,
@@ -25,10 +26,44 @@ conn = psycopg2.connect(
 
 class DBConn:
 
-    conn: psycopg2.connection
-
     def __init__(self):
         self.conn = conn
+
+    def save_user_data(self, userid, uname, password, email):
+
+        with self.conn.cursor() as cur:
+            cur.execute("UPDATE users SET name = name - %s WHERE id = %s",
+                        (uname, userid))
+        self.conn.commit()
+
+        with self.conn.cursor() as cur:
+            cur.execute("UPDATE users SET password = password - %s WHERE id = %s",
+                        (password, userid))
+        self.conn.commit()
+
+        with self.conn.cursor() as cur:
+            cur.execute("UPDATE users SET email = email- %s WHERE id = %s",
+                        (email, userid))
+        self.conn.commit()
+
+    def export_user_data(self) -> Account:
+        with conn.cursor() as cur:
+            DATA = []
+            cur.execute("SELECT * FROM userinfo")
+            rows = cur.fetchall()
+            for row in rows:
+                DATA.append(row)
+
+            acc = Account(DATA[0], DATA[2], DATA[3], DATA[1])
+
+            return acc
+
+
+
+
+
+
+
 
 
 
--- backend/src/dbconfig.sql
+++ backend/src/dbconfig.sql
@@ -9,3 +9,11 @@ GRANT CREATE ON DATABASE defaultdb TO users;
 DROP SCHEMA IF EXISTS warrenschema CASCADE;
 CREATE SCHEMA warrenschema AUTHORIZATION warren;
 GRANT USAGE ON SCHEMA warrenschema TO uofthax8hack;
+
+
+CREATE TABLE defaultdb.warrenschema.userinfo (
+                                                 userid INT,
+                                                 name STRING,
+                                                 email STRING,
+                                                 password STRING
+);
